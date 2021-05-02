8d8e14f7eca9c310e3e2dd51e3e9c5a5d8ad2e53
JurievdS <rocker@live.co.za>
2021-05-03 00:20:37 +0200

    App rework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import Login from "./components/auth/Login";
-import Routes from "./components/routing/Routes";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route component={Routes} />
-          </Switch>
-        </Fragment>
-      </Router>
-    </Provider>
+    <Router>
+      <Route path="/Dashboard" component={Dashboard}/>
+    
+
+    </Router>
   );
 }
 
