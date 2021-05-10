e10e5d71a9797fed43ebe1d60659085c1b8b121b
JurievdS <rocker@live.co.za>
2021-05-10 06:21:14 +0200

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./Pages/Login";
+import Routes from "./routing/Routes"
 
 function App() {
   return (
     <Router>
       <main>
-        <Route path="/" component={Login} />
-        {/* <Route path="/Dashboard" component={Dashboard} /> */}
+        <Route path="/" component={Dashboard} />
+        <Routes component={Routes} />
       </main>
     </Router>
   );
